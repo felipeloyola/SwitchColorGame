f2b190da734500f423690a2bacfa17d29e150acb
Felipe Loyola <felipe.l@funxperience.com>
2019-09-23 13:35:11 -0300

    First Commit

---
+++ LabGame1/Controllers/GameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  LabGame1
+//
+//  Created by Felipe Loyola on 9/23/19.
+//  Copyright © 2019 Felipe Loyola. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            let scene = GameScene(size: view.bounds.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+                
+            // Present the scene
+            view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+
+}
---
+++ LabGame1/Templates/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  LabGame1
+//
+//  Created by Felipe Loyola on 9/23/19.
+//  Copyright © 2019 Felipe Loyola. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    
+    static let none:UInt32 = 0
+    static let ballCategory:UInt32 = 0x1 //1
+    static let switchCategory:UInt32 = 0x1 << 1 // 10
+
+}
